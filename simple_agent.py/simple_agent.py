85dda5480f74c32b72f8c7c5d4c1d578463165eb
Rohma-Shabbir <168419231+RohmaShabbir@users.noreply.github.com>
2025-08-13 15:18:25 +0500

    Add files via upload

---
+++ simple_agent.py/simple_agent.py
@@ -0,0 +1,28 @@
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=client
+)
+
+agent = Agent(
+    name="Assistant",
+    instructions="are you helpful assistant",
+    model=model
+)
+
+prompt = input("What can I help You? ")
+
+result = Runner.run_sync(agent, prompt)
+print(result.final_output)
